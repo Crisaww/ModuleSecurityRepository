1b13c6e9ad1eba619d923847894cd809dabd5f56
Cristian Narváez <cristianfns11@gmail.com>
2024-09-05 17:58:18 -0500

    Hacer lo de cisharp

--- ModuleSecurity/Entity/obj/Debug/net8.0/Entity.AssemblyInfo.cs
+++ ModuleSecurity/Entity/obj/Debug/net8.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1efdf4ee8377a278e50df8a698ab21b3ae179b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaad8fe1c18f18e82f4ffb76adc3cceecc735081")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
